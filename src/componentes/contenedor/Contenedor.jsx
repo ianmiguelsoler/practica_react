5d88309b4813ff8587c2ea754409aa9733717aa6
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-30 19:32:04 +0200

    ejercicio 1 react

---
+++ src/componentes/contenedor/Contenedor.jsx
@@ -0,0 +1,14 @@
+import './Contenedor.css'
+
+function Contenedor() {
+  
+  return (
+    <>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default Contenedor
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import Contenedor from './componentes/contenedor/Contenedor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Contenedor />
   </StrictMode>,
 )
