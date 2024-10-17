7ae39469524ece7366cb8c8a71bed60b7cdce960
ianmiguelsoler <ianmiguel@gmail.com>
2024-10-17 17:25:46 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Parrafico from './componentes/Parrafico.jsx';
 import Contadorico from './componentes/Contadorico.jsx';
+import Numericos from './componentes/Numericos.jsx';
 
 const App = () => {
   return (
+    <>
+    <Numericos />
+    <Parrafico />
     <Contadorico />
+    </>
   );
 };
 
--- src/componentes/Numericos.jsx
+++ src/componentes/Numericos.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { anadirNumero } from "../biblioteca/biblioteca.js";
+import { obtenerNumeroAleatorio } from "../biblioteca/biblioteca.js";
 
 const Numericos = () => {
     
