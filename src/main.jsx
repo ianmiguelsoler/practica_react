b303eda6006e0d8d4b22b9a85a2461bbfd6e3d33
ianmiguelsoler <ianmiguel@gmail.com>
2024-10-17 16:03:06 +0200

    limpieza 2

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import AppPeliculas from './AppPeliculas.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppPeliculas />
+    <App />
   </StrictMode>,
 )
