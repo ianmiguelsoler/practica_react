e6b5aa6855069c6788523e336e3392c12f66f585
ianmiguelsoler <ianmiguel@gmail.com>
2024-10-17 21:07:58 +0200

    preparado para hacer el ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css';
 // import Contadorico from './componentes/Contadorico.jsx';
 // import Numericos from './componentes/Numericos.jsx';
 import Peliculas from './componentes/ejercicio_2/Peliculas/Peliculas.jsx';
+import peliculasData from './informacionBDD/peliculas.json';
+
 
 const App = () => {
-  return (
-    <>
-    {
-    /* <Numericos />
+   /* <Numericos />
     <Parrafico />
     <Contadorico /> */
-    <Peliculas/>
-    }
+  return (
+    <>
+      <h1>Contenido dentro del contenedor</h1>
+      <Peliculas listado={peliculasData.peliculas}/>
     </>
   );
 };
--- src/componentes/ejercicio_2/Peliculas/Peliculas.jsx
+++ src/componentes/ejercicio_2/Peliculas/Peliculas.jsx
@@ -1,30 +1,29 @@
 import Pelicula from '../pelicula/Pelicula.jsx';
-import peliculasData from '../../../informacionBDD/peliculas.json'; 
 
 const Peliculas = (props) => {
   const { listado } = props;
   return (
     <>
     <div className="peliculas">
-      {listado && listado.peliculas && listado.peliculas.length > 0 ? (
-        listado.peliculas.map((pelicula) => (
-          <Pelicula
-            key={pelicula.id}
-            nombre={pelicula.nombre}
-            cartelera={pelicula.cartelera}
-            director={pelicula.director}
-            actores={pelicula.actores}
-          >
-            {pelicula.resumen}
-          </Pelicula>
-        ))
-      ) : (
+    {listado.length
+          ? listado.map((pelicula) => {
+              return (
+                <Pelicula
+                nombre={pelicula.nombre}
+                cartelera={pelicula.cartelera}
+                director={pelicula.director}
+                actores={pelicula.actores}
+                >
+                  {pelicula.resumen}
+                </Pelicula>
+              );
+            })
+          : (
         <p>No hay películas disponibles</p>
       )}
     </div>
     </>
   );
-  
 };
 
 export default Peliculas;
