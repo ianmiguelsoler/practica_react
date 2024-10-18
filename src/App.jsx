c2f08619886c2555df5c6a567dccec2ffbfcaaf7
ianmiguelsoler <ianmiguel@gmail.com>
2024-10-18 20:01:01 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css';
-// import Parrafico from './componentes/Parrafico.jsx';
-// import Contadorico from './componentes/Contadorico.jsx';
-// import Numericos from './componentes/Numericos.jsx';
 import Peliculas from './componentes/ejercicio_2/Peliculas/Peliculas.jsx';
 import peliculasData from './informacionBDD/peliculas.json';
 
 
 const App = () => {
-   /* <Numericos />
-    <Parrafico />
-    <Contadorico /> */
+
   return (
     <>
       <h1>Contenido dentro del contenedor</h1>
