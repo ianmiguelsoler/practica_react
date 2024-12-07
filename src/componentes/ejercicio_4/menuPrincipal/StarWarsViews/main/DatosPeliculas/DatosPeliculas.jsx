bb0db3ac74bc7decd3ea8a79d359d3d3a6acde3b
ianmiguelsoler <ianmiguel@gmail.com>
2024-12-07 15:39:49 +0100

    terminado

--- src/componentes/ejercicio_4/menuPrincipal/StarWarsViews/main/DatosPeliculas/DatosPeliculas.jsx
+++ src/componentes/ejercicio_4/menuPrincipal/StarWarsViews/main/DatosPeliculas/DatosPeliculas.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./DatosPeliculas.css";
 
 const DatosPelicula = ({ pelicula }) => {
   const convertirFechaAEuropea = (fecha) => {
@@ -7,15 +8,28 @@ const DatosPelicula = ({ pelicula }) => {
   };
 
   return (
-    <>
     <div className="datos-pelicula">
-      <h2>{pelicula.title}</h2>
-      <p><strong>Director:</strong> {pelicula.director}</p>
-      <p><strong>Productor:</strong> {pelicula.producer}</p>
-      <p><strong>Fecha de lanzamiento:</strong> {convertirFechaAEuropea(pelicula.release_date)}</p>
-      <p><strong>Sinopsis:</strong> {pelicula.opening_crawl}</p>
+      <div className="encabezado">
+        <img src="src\biblioteca\videos\naveImperial.png" alt="Nave Imperial" className="nave-imperial" />
+        <h2 className="titulo-pelicula">{pelicula.title}</h2>
+      </div>
+      <div className="contenido">
+        <p>
+          <strong>🧑‍🎬 Director:</strong> {pelicula.director}
+        </p>
+        <p>
+          <strong>🎬 Productor:</strong> {pelicula.producer}
+        </p>
+        <p>
+          <strong>📅 Fecha de lanzamiento:</strong> {convertirFechaAEuropea(
+            pelicula.release_date
+          )}
+        </p>
+        <p className="sinopsis">
+          <strong>📜 Sinopsis:</strong> {pelicula.opening_crawl}
+        </p>
+      </div>
     </div>
-    </>
   );
 };
 
