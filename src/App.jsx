46ca9a1d40750fc3f5a5d276b014d1fc97c8a939
ianmiguelsoler <ianmiguel@gmail.com>
2024-12-09 22:21:22 +0100

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
-import React, { useState } from "react";
+import React from "react";
 import Contenedor from './componentes/ejercicio_4/app/contenedor/Contenedor.jsx';
-import DiscoFormulario from './componentes/ejercicio_4/menuPrincipal/discosFormulario/DiscoFormulario.jsx';
 import ListaStarWarsApi from './componentes/ejercicio_4/menuPrincipal/StarWarsViews/main/ListaStarWarsApi.jsx';
 const App = () => {
 
