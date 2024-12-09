9b7d15748b5d59019d540a2d2f68a04d83e11287
ianmiguelsoler <ianmiguel@gmail.com>
2024-12-10 00:48:24 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from "react";
 import Contenedor from './componentes/ejercicio_4/app/contenedor/Contenedor.jsx';
-import ListaStarWarsApi from './componentes/ejercicio_4/menuPrincipal/StarWarsViews/main/ListaStarWarsApi.jsx';
+import ListaStarWarsApi from './componentes/ejercicio_4/menuPrincipal/NasaApp/main/ListaStarWarsApi.js';
 const App = () => {
 
   return (
